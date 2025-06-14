532ef59d1726ca16879410f39481b34820a2f89b
flifez <flif3x@gmail.com>
2025-06-15 00:18:08 +0800

    Initial Commit

---
+++ Geometry/GeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryApp.swift
+//  Geometry
+//
+//  Created by FlifeX on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct GeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
